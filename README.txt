9d3230735a72623f91f952194b38c32bc0825fea
mamparo <myronamparo@gmail.com>
2014-05-13 14:52:06 -0700

    Version 1.0 is released
    Finished the code and added new updates to the read me, Added user
    input and revision histories up till 1.0

--- README.txt
+++ README.txt
@@ -1,18 +1,27 @@
-Visualizer.py Version 0.7 04/30/2014
+Visualizer.py Version 1.0 05/13/2014
 
 SUMMARY
 -------
--Takes audio files and grabs the samples from the audio files
+Framework:
+-Takes audio files and grabs the samples from the audio files from a user input
 
--Finds the highest and the lowest sample and creates sets of data
+-Finds the highest and the lowest sample and creates sets of data from the music file provided by the user
 
+Matt:
+-Created a color visualizer utilizing some of the information given from the sample
 -Displays colors based on regions in the data
+-Developed sound ranges 
 
+Myron:
 -Bar visualizer added to the code
+-Displays bars based on how high or low the music plays.
+-Developed the screen implementation
+-Added a user input for exiting the screen
+-implemented functions and user interface
 
 
-REVISIONS
-—---—--—-
+REVISION HISTORY
+—---—--—--—--—--
 V0.1
 -Started with courageousilluminations code
 V0.2
@@ -27,11 +36,18 @@ V0.6
 -Color visualizer being made
 V0.7
 -Bar visualizer being made and needs to be debuggeg
+V0.8
+-More bars added to the screen
+V0.9
+-Added the Color visualizer
+V1.0
+-Encapsulated all items into functions and created a main
+-Added user input to choose their own song files
+
 
 INSTALLATION
 ------------
-Please install PYGAME 1.9, NUMPY, and PYTHON 2.7 before you run the program. Double click the .py file
-to run the program.
+Please install PYGAME 1.9, NUMPY, and PYTHON 2.7 before you run the program. Double click the .py file to run the program.
 
 
 CONTACT INFO
--- Visualizer.py
+++ Visualizer.py
@@ -5,131 +5,316 @@ import numpy as np
 import time
 import wave 
 import array
+from random import randint
 
 pgame.mixer.init() #initalizes the mixer element
 pgame.init() #initalizes the game screen
-
-#loads the music into pygame
-pgame.mixer.music.load('ArchieCutMono.wav')
-sound_file = wave.open('ArchieCutMono.wav')
-data = sound_file.readframes(44100*15) #takes the first 15 seconds of the song
-
-
-print sound_file.getparams()
-stringOfBytes = np.fromstring(data, '<H')
-#'<H' = little endian unsigned short
-#needed to grab the data from the sound
-
 lowest = 90000
 highest = 0
 
-#reads into the string of bytes and gathers the highest and lowest elements
-for i in range(0, len(stringOfBytes), 882):
-    print stringOfBytes[i]
-    if (stringOfBytes[i] > highest):
-        high = stringOfBytes[i]
-    if (stringOfBytes[i] < lowest):
-        low = stringOfBytes[i]
+def pickMusic():
+    music = str(raw_input('Enter the name of the song: '))
+    return music
 
+def framework(string):
+    #loads the music into pygame
+    pgame.mixer.music.load(string + '.wav')
+    sound_file = wave.open(string + '.wav')
+    data = sound_file.readframes(44100*10) #takes the first 10 seconds of the song
+    
+    print sound_file.getparams()
+    stringOfBytes = np.fromstring(data, '<H')
+    #'<H' = little endian unsigned short
+    #needed to grab the data from the sound
 
-print 'done'
+    return stringOfBytes
 
-print low
-print high
-i=0
+def highestRange(highest, stringOfBytes):
+    #reads into the string of bytes and gathers the highest and lowest elements
+    for i in range(0, len(stringOfBytes), 882):
+        if (stringOfBytes[i] > highest):
+            highest = stringOfBytes[i]
+    return highest
 
-#separates the sample ranges into sections
-rangeOf = high - low
-divisionOf = rangeOf/6
-first = 0
-second = divisionOf + first
-third = second + divisionOf
-fourth = third + divisionOf
-fifth = fourth + divisionOf
-sixth = fifth + divisionOf
+def lowestRange(lowest, stringOfBytes):
+    #reads into the string of bytes and gathers the highest and lowest elements
+    for i in range(0, len(stringOfBytes), 882):
+        if (stringOfBytes[i] < lowest):
+            lowest = stringOfBytes[i]
+    return lowest
 
-
-print first, second, third, fourth, fifth, sixth
-
-for i in range(0, len(stringOfBytes), 882):
-
-    if stringOfBytes[i] <= first:
-        print '0 or less'
-
-    elif first < stringOfBytes[i] <= second:
-        print 'first'
-
-    elif second < stringOfBytes[i] <= third:
-        print 'second'
-
-    elif third < stringOfBytes[i] <= fourth:
-        print 'third'
-
-    elif fourth < stringOfBytes[i] <= fifth:
-        print 'fourth'
-
-    elif fifth < stringOfBytes[i] <= sixth:
-        print 'fifth'
-    else:
-        print 'sixth'
-
-
-
-print first, second, third, fourth, fifth, sixth
-
-pgame.display.init()#initializes the display element
-screen = pgame.display.set_mode([640,480])
-pgame.display.set_caption("Visualizer")
-WHITE = (255, 255, 255)
-black = (0,0,0)
-screen.fill(WHITE)
-done = False
-tick = time.time()
-arrayStep = 0
-while not done:
-    #draws the bars
-    pgame.draw.rect(screen, black, [30,240,150,0])
-                                   #x,y,width,height
-    pgame.draw.rect(screen, black, [180,240,150,0])
-    pgame.draw.rect(screen, black, [330,240,150,0])
+def barsVisualizer(highest, lowest, stringOfBytes):
+    #separates the sample ranges into sections
+    rangeOf = highest - lowest
+    divisionOf = rangeOf/6
+    first = 0
+    second = divisionOf + first
+    third = second + divisionOf
+    fourth = third + divisionOf
+    fifth = fourth + divisionOf
+    sixth = fifth + divisionOf
+    print first, second, third, fourth, fifth, sixth
+    
+    screen = pgame.display.set_mode([640,480])
+    WHITE = (255, 255, 255)
+    black = (0,0,0)
+    done = False
+    tick = time.time()
+    arrayStep = 0
+    pgame.display.init()#initializes the display element
+    pgame.display.set_caption("Visualizer")
     screen.fill(WHITE)
-    for event in pgame.event.get():
-        if event.type == pgame.QUIT:
-            done = True
-    pgame.draw.aaline(screen, black, [20, 240], [620, 240], True)
-    if stringOfBytes[arrayStep] <= first:
-        pgame.draw.rect(screen, black, [30,240,150,0])
-        pgame.draw.rect(screen, black, [180,240,150,0])
-        pgame.draw.rect(screen, black, [330,240,150,0])
-    elif first < stringOfBytes[arrayStep] <= second:
-        pgame.draw.rect(screen, black, [30,240,150,0])
-        pgame.draw.rect(screen, black, [180,240,150,10])
-        pgame.draw.rect(screen, black, [330,240,150,0])
-    elif second < stringOfBytes[arrayStep] <= third:
-        pgame.draw.rect(screen, black, [30,240,150,50])
-        pgame.draw.rect(screen, black, [180,240,150,100])
-        pgame.draw.rect(screen, black, [330,240,150,50])
-    elif third < stringOfBytes[arrayStep] <= fourth:
-        pgame.draw.rect(screen, black, [30,240,150,75])
-        pgame.draw.rect(screen, black, [180,240,150,200])
-        pgame.draw.rect(screen, black, [330,240,150,75])
-    elif fourth < stringOfBytes[arrayStep] <= fifth:
-        pgame.draw.rect(screen, black, [30,240,150,0])
-        pgame.draw.rect(screen, black, [180,240,150,-10])
-        pgame.draw.rect(screen, black, [330,240,150,0])
-    elif fifth < stringOfBytes[arrayStep] <= sixth:
-        pgame.draw.rect(screen, black, [30,240,150,-50])
-        pgame.draw.rect(screen, black, [180,240,150,-100])
-        pgame.draw.rect(screen, black, [330,240,150,-50])
-    else:
-        pgame.draw.rect(screen, black, [30,240,150,-75])
-        pgame.draw.rect(screen, black, [180,240,150,-200])
-        pgame.draw.rect(screen, black, [330,240,150,-75])
-    pgame.display.update()
-    time.sleep(0.02)
-    arrayStep += 1
+    pgame.mixer.music.play()
+    while not done:
+        randomNumber = randint(0,4)
+        screen.fill(WHITE)
+        for event in pgame.event.get():
+            if event.type == pgame.QUIT:
+                done = True
+        if stringOfBytes[arrayStep] <= first:
+            pgame.draw.aaline(screen, black, [20, 240], [620, 240], True)
+            if(randomNumber == 0):
+                pgame.draw.rect(screen, black, [30,240,20,0])
+                pgame.draw.rect(screen, black, [60,240,20,0])
+                pgame.draw.rect(screen, black, [90,240,20,0])
+                pgame.draw.rect(screen, black, [120,240,20,0])
+            elif(randomNumber == 1):
+                pgame.draw.rect(screen, black, [150,240,20,0])
+                pgame.draw.rect(screen, black, [180,240,20,0])
+                pgame.draw.rect(screen, black, [210,240,20,0])
+                pgame.draw.rect(screen, black, [240,240,20,0])
+            elif(randomNumber == 2):
+                pgame.draw.rect(screen, black, [270,240,20,0])
+                pgame.draw.rect(screen, black, [300,240,20,0])
+                pgame.draw.rect(screen, black, [330,240,20,0])
+                pgame.draw.rect(screen, black, [360,240,20,0])
+            elif(randomNumber == 3):
+                pgame.draw.rect(screen, black, [390,240,20,0])
+                pgame.draw.rect(screen, black, [420,240,20,0])
+                pgame.draw.rect(screen, black, [450,240,20,0])
+                pgame.draw.rect(screen, black, [480,240,20,0])
+            else:
+                pgame.draw.rect(screen, black, [510,240,20,0])
+                pgame.draw.rect(screen, black, [540,240,20,0])
+                pgame.draw.rect(screen, black, [570,240,20,0])
+                pgame.draw.rect(screen, black, [600,240,20,0]) 
+        elif first < stringOfBytes[arrayStep] <= second:
+            pgame.draw.aaline(screen, black, [20, 240], [620, 240], True)
+            if(randomNumber == 0):
+                pgame.draw.rect(screen, black, [30,240,20,0])
+                pgame.draw.rect(screen, black, [60,240,20,20])
+                pgame.draw.rect(screen, black, [90,240,20,20])
+                pgame.draw.rect(screen, black, [120,240,20,0])
+            elif(randomNumber == 1):
+                pgame.draw.rect(screen, black, [150,240,20,0])
+                pgame.draw.rect(screen, black, [180,240,20,20])
+                pgame.draw.rect(screen, black, [210,240,20,20])
+                pgame.draw.rect(screen, black, [240,240,20,0])
+            elif(randomNumber == 2):
+                pgame.draw.rect(screen, black, [270,240,20,0])
+                pgame.draw.rect(screen, black, [300,240,20,20])
+                pgame.draw.rect(screen, black, [330,240,20,20])
+                pgame.draw.rect(screen, black, [360,240,20,0])
+            elif(randomNumber == 3):
+                pgame.draw.rect(screen, black, [390,240,20,0])
+                pgame.draw.rect(screen, black, [420,240,20,20])
+                pgame.draw.rect(screen, black, [450,240,20,20])
+                pgame.draw.rect(screen, black, [480,240,20,0])
+            else:
+                pgame.draw.rect(screen, black, [510,240,20,0])
+                pgame.draw.rect(screen, black, [540,240,20,20])
+                pgame.draw.rect(screen, black, [570,240,20,20])
+                pgame.draw.rect(screen, black, [600,240,20,0]) 
+        elif second < stringOfBytes[arrayStep] <= third:
+            pgame.draw.aaline(screen, black, [20, 240], [620, 240], True)
+            if(randomNumber == 0):
+                pgame.draw.rect(screen, black, [30,240,20,20])
+                pgame.draw.rect(screen, black, [60,240,20,50])
+                pgame.draw.rect(screen, black, [90,240,20,50])
+                pgame.draw.rect(screen, black, [120,240,20,20])
+            elif(randomNumber == 1):
+                pgame.draw.rect(screen, black, [150,240,20,20])
+                pgame.draw.rect(screen, black, [180,240,20,50])
+                pgame.draw.rect(screen, black, [210,240,20,50])
+                pgame.draw.rect(screen, black, [240,240,20,20])
+            elif(randomNumber == 2):
+                pgame.draw.rect(screen, black, [270,240,20,20])
+                pgame.draw.rect(screen, black, [300,240,20,50])
+                pgame.draw.rect(screen, black, [330,240,20,50])
+                pgame.draw.rect(screen, black, [360,240,20,20])
+            elif(randomNumber == 3):
+                pgame.draw.rect(screen, black, [390,240,20,20])
+                pgame.draw.rect(screen, black, [420,240,20,50])
+                pgame.draw.rect(screen, black, [450,240,20,50])
+                pgame.draw.rect(screen, black, [480,240,20,20])
+            else:
+                pgame.draw.rect(screen, black, [510,240,20,20])
+                pgame.draw.rect(screen, black, [540,240,20,50])
+                pgame.draw.rect(screen, black, [570,240,20,50])
+                pgame.draw.rect(screen, black, [600,240,20,20]) 
+        elif third < stringOfBytes[arrayStep] <= fourth:
+            pgame.draw.aaline(screen, black, [20, 240], [620, 240], True)
+            if(randomNumber == 0):
+                pgame.draw.rect(screen, black, [30,240,20,50])
+                pgame.draw.rect(screen, black, [60,240,20,100])
+                pgame.draw.rect(screen, black, [90,240,20,100])
+                pgame.draw.rect(screen, black, [120,240,20,50])
+            elif(randomNumber == 1):
+                pgame.draw.rect(screen, black, [150,240,20,50])
+                pgame.draw.rect(screen, black, [180,240,20,100])
+                pgame.draw.rect(screen, black, [210,240,20,100])
+                pgame.draw.rect(screen, black, [240,240,20,50])
+            elif(randomNumber == 2):
+                pgame.draw.rect(screen, black, [270,240,20,50])
+                pgame.draw.rect(screen, black, [300,240,20,100])
+                pgame.draw.rect(screen, black, [330,240,20,100])
+                pgame.draw.rect(screen, black, [360,240,20,50])
+            elif(randomNumber == 3):
+                pgame.draw.rect(screen, black, [390,240,20,50])
+                pgame.draw.rect(screen, black, [420,240,20,100])
+                pgame.draw.rect(screen, black, [450,240,20,100])
+                pgame.draw.rect(screen, black, [480,240,20,50])
+            else:
+                pgame.draw.rect(screen, black, [510,240,20,50])
+                pgame.draw.rect(screen, black, [540,240,20,100])
+                pgame.draw.rect(screen, black, [570,240,20,100])
+                pgame.draw.rect(screen, black, [600,240,20,50]) 
+        elif fourth < stringOfBytes[arrayStep] <= fifth:
+            pgame.draw.aaline(screen, black, [20, 240], [620, 240], True)
+            if(randomNumber == 0):
+                pgame.draw.rect(screen, black, [30,240,20,0])
+                pgame.draw.rect(screen, black, [60,240,20,-20])
+                pgame.draw.rect(screen, black, [90,240,20,-20])
+                pgame.draw.rect(screen, black, [120,240,20,0])
+            elif(randomNumber == 1):
+                pgame.draw.rect(screen, black, [150,240,20,0])
+                pgame.draw.rect(screen, black, [180,240,20,-20])
+                pgame.draw.rect(screen, black, [210,240,20,-20])
+                pgame.draw.rect(screen, black, [240,240,20,0])
+            elif(randomNumber == 2):
+                pgame.draw.rect(screen, black, [270,240,20,0])
+                pgame.draw.rect(screen, black, [300,240,20,-20])
+                pgame.draw.rect(screen, black, [330,240,20,-20])
+                pgame.draw.rect(screen, black, [360,240,20,0])
+            elif(randomNumber == 3):
+                pgame.draw.rect(screen, black, [390,240,20,0])
+                pgame.draw.rect(screen, black, [420,240,20,-20])
+                pgame.draw.rect(screen, black, [450,240,20,-20])
+                pgame.draw.rect(screen, black, [480,240,20,0])
+            else:
+                pgame.draw.rect(screen, black, [510,240,20,0])
+                pgame.draw.rect(screen, black, [540,240,20,-20])
+                pgame.draw.rect(screen, black, [570,240,20,-20])
+                pgame.draw.rect(screen, black, [600,240,20,0])
+        elif fifth < stringOfBytes[arrayStep] <= sixth:
+            pgame.draw.aaline(screen, black, [20, 240], [620, 240], True)
+            if(randomNumber == 0):
+                pgame.draw.rect(screen, black, [30,240,20,-20])
+                pgame.draw.rect(screen, black, [60,240,20,-50])
+                pgame.draw.rect(screen, black, [90,240,20,-50])
+                pgame.draw.rect(screen, black, [120,240,20,-20])
+            elif(randomNumber == 1):
+                pgame.draw.rect(screen, black, [150,240,20,-20])
+                pgame.draw.rect(screen, black, [180,240,20,-50])
+                pgame.draw.rect(screen, black, [210,240,20,-50])
+                pgame.draw.rect(screen, black, [240,240,20,-20])
+            elif(randomNumber == 2):
+                pgame.draw.rect(screen, black, [270,240,20,-20])
+                pgame.draw.rect(screen, black, [300,240,20,-50])
+                pgame.draw.rect(screen, black, [330,240,20,-50])
+                pgame.draw.rect(screen, black, [360,240,20,-20])
+            elif(randomNumber == 3):
+                pgame.draw.rect(screen, black, [390,240,20,-20])
+                pgame.draw.rect(screen, black, [420,240,20,-50])
+                pgame.draw.rect(screen, black, [450,240,20,-50])
+                pgame.draw.rect(screen, black, [480,240,20,-20])
+            else:
+                pgame.draw.rect(screen, black, [510,240,20,-20])
+                pgame.draw.rect(screen, black, [540,240,20,-50])
+                pgame.draw.rect(screen, black, [570,240,20,-50])
+                pgame.draw.rect(screen, black, [600,240,20,-20])
+        else:
+            pgame.draw.aaline(screen, black, [20, 240], [620, 240], True)
+            if(randomNumber == 0):
+                pgame.draw.rect(screen, black, [30,240,20,-50])
+                pgame.draw.rect(screen, black, [60,240,20,-100])
+                pgame.draw.rect(screen, black, [90,240,20,-100])
+                pgame.draw.rect(screen, black, [120,240,20,-50])
+            elif(randomNumber == 1):
+                pgame.draw.rect(screen, black, [150,240,20,-50])
+                pgame.draw.rect(screen, black, [180,240,20,-100])
+                pgame.draw.rect(screen, black, [210,240,20,-100])
+                pgame.draw.rect(screen, black, [240,240,20,-50])
+            elif(randomNumber == 2):
+                pgame.draw.rect(screen, black, [270,240,20,-50])
+                pgame.draw.rect(screen, black, [300,240,20,-100])
+                pgame.draw.rect(screen, black, [330,240,20,-100])
+                pgame.draw.rect(screen, black, [360,240,20,-50])
+            elif(randomNumber == 3):
+                pgame.draw.rect(screen, black, [390,240,20,-50])
+                pgame.draw.rect(screen, black, [420,240,20,-100])
+                pgame.draw.rect(screen, black, [450,240,20,-100])
+                pgame.draw.rect(screen, black, [480,240,20,-50])
+            else:
+                pgame.draw.rect(screen, black, [510,240,20,-50])
+                pgame.draw.rect(screen, black, [540,240,20,-100])
+                pgame.draw.rect(screen, black, [570,240,20,-100])
+                pgame.draw.rect(screen, black, [600,240,20,-50])
+        pgame.display.update()
+        time.sleep(0.05)
+        arrayStep += 1
+
+    pgame.display.quit()#exits the display
+    pgame.quit() #exits the module
+
+def colorVisualizer(highest, lowest, stringOfBytes):
+    #separates the sample ranges into sections
+    rangeOf = highest - lowest
+    divisionOf = rangeOf/6
+    first = 0
+    second = divisionOf + first
+    third = second + divisionOf
+    fourth = third + divisionOf
+    fifth = fourth + divisionOf
+    sixth = fifth + divisionOf
+    print first, second, third, fourth, fifth, sixth
+    
+    screen = pgame.display.set_mode([640,480])
+    pgame.display.init()#initializes the display element
+    pgame.display.set_caption("Visualizer")
+    screen.fill(WHITE)
+    pgame.mixer.music.play()
+
+    for i in range(0, len(stringOfBytes), 882):
+        screen.fill(WHITE)
+        if stringOfBytes[i] <= first:
+            pgame.draw.circle(screen, (255, 255, 255), (320,240), 100)
+        elif first < stringOfBytes[i] <= second:
+            pgame.draw.circle(screen, (220, 220, 20), (320,240), 110)
+        elif second < stringOfBytes[i] <= third:
+            pgame.draw.circle(screen, (190, 90, 190), (320,240), 120)
+        elif third < stringOfBytes[i] <= fourth:
+            pgame.draw.circle(screen, (250, 150, 150), (320,240), 130)
+        elif fourth < stringOfBytes[i] <= fifth:
+            pgame.draw.circle(screen, (120, 200, 120), (320,240), 140)
+        elif fifth < stringOfBytes[i] <= sixth:
+            pgame.draw.circle(screen, (90, 90, 210), (320,240), 150)
+        else:
+            pgame.draw.circle(screen, (240, 50, 50), (320,240), 160)
+        pgame.display.update()
+        time.sleep(0.02)
+
+    pgame.display.quit()#exits the display
+    pgame.quit() #exits the module
 
 
+def main():
+    music = pickMusic()
+    stringBytes = framework(music)
+    highRange = highestRange(highest, stringBytes)
+    lowRange = lowestRange(lowest, stringBytes)
+    colorVisualizer(highRange, lowRange, stringBytes)
+    #barsVisualizer(highRange, lowRange, stringBytes)
 
-pgame.display.quit()#exits the display
-pgame.quit() #exits the module
+
+    
